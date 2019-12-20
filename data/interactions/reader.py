1c135ac9f9a26af84aee6ee7d036cd17a1559bbc
Abel Pacheco-Ortega <dougbel.unam@gmail.com>
2019-12-20 11:57:36 +0000

    Reader updated for reading csv file with trained ibs

--- data/interactions/reader.py
+++ data/interactions/reader.py
@@ -1,20 +1,24 @@
 import trimesh
 import numpy as np
 import pandas as pd
+import os
 
 if __name__ == '__main__':
-    # interactions_data = pd.read_csv("./data/interactions/motorbike_rider/")
+    interactions_data = pd.read_csv("./data/interactions/interaction.csv")
 
     to_read = 'ride'
-    # interaction = interactions_data[interactions_data['interaction'] == to_read]
+    interaction = interactions_data[interactions_data['interaction'] == to_read]
 
-    tri_mesh_env = trimesh.load_mesh("./data/interactions/motorbike_rider/motorbike.ply")
-    tri_mesh_obj = trimesh.load_mesh("./data/interactions/motorbike_rider/rider.ply")
-    tri_mesh_ibs_segmented = trimesh.load_mesh("./data/interactions/motorbike_rider/ibs_motorbike_biker_sampled_600_resamplings_2.ply")
+    tri_mesh_env = trimesh.load_mesh(os.path.join(interaction.iloc[0]['directory'],
+                                                  interaction.iloc[0]['tri_mesh_env']))
+    tri_mesh_obj = trimesh.load_mesh(os.path.join(interaction.iloc[0]['directory'],
+                                                  interaction.iloc[0]['tri_mesh_obj']))
+    tri_mesh_ibs_segmented = trimesh.load_mesh(os.path.join(interaction.iloc[0]['directory'],
+                                               interaction.iloc[0]['tri_mesh_ibs_segmented']))
 
     tri_mesh_obj.visual.face_colors = [0, 255, 0, 100]
     tri_mesh_env.visual.face_colors = [100, 100, 100, 100]
-    tri_mesh_ibs_segmented.visual.face_colors = [0, 0, 150, 100]
+    tri_mesh_ibs_segmented.visual.face_colors = [0, 0, 255, 100]
 
     visualizer = trimesh.Scene([
         tri_mesh_obj,
@@ -22,4 +26,4 @@ if __name__ == '__main__':
         tri_mesh_ibs_segmented,
     ])
 
-    visualizer.show()
+    visualizer.show(flags={'cull': False, 'wireframe': False, 'axis': False})
